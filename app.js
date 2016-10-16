02081daf164223b9a3daca1520caa9bad3488a59
Russell Knox <russell.knox@yahoo.co.uk>
2016-10-16 16:20:22 +0100

    test

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.set('view engine', 'handlebars')
 app.use('/', express.static(__dirname + '/assets'))
 
 routes(app)
-
+h
 const PORT = process.env.PORT || 3000
 app.listen(3000, () => console.log('listening on port: ', PORT) )
